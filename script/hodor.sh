7d51808e5439bbe6761e285bc6b58fd6f76d9220
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-12 16:30:06 +0100

    Check if the user is root and send HODOOR to everyone

--- script/hodor.sh
+++ script/hodor.sh
@@ -2,8 +2,14 @@
 # When used inode is > 80% archive all log file in /var/log/exercice/archives with a unique name 
 # Print "HODOOOR !!!" to all term 
 
+if [ ! $USER = root ]; then
+    echo "You need to be root"
+fi
+
 percent_inode_use=$(df -i /dev/sda3 | tail -n 1 | awk '{print substr($5, 1, length($5)-1)}')
 
-if [ $percent_inode_use -gt 3 ]; then 
-    
+if [ $percent_inode_use -gt 3 ]; then
+    for pts in /dev/pts/*; do
+        echo "HODOOOR !!!" > $pts   
+    done  
 fi

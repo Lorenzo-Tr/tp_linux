0ea2d1dbed38788b249e2c97549885397c15ffa8
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-12 15:40:25 +0100

    Check if percent_inode_use is greater then a number

--- script/hodor.sh
+++ script/hodor.sh
@@ -2,3 +2,8 @@
 # When used inode is > 80% archive all log file in /var/log/exercice/archives with a unique name 
 # Print "HODOOOR !!!" to all term 
 
+percent_inode_use=$(df -i /dev/sda3 | tail -n 1 | awk '{print substr($5, 1, length($5)-1)}')
+
+if [ $percent_inode_use -gt 3 ]; then 
+    
+fi

b375e2bf93197071e4d432296002d4946179947d
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-12 12:31:24 +0100

    Create a temp file and insert the 10 random characters in it

--- script/log.sh
+++ script/log.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # This script write 10 caraters from /dev/random in n files
 # Each files have a unique name and will be save in /var/log/exercise/recent_logs
+if [ "$USER" = "root" ]
+then
+    echo 
+else
+    echo You need to be root
+fi
 
-sudo mkdir -p /var/log/exercice/recent_logs/
+mkdir -p /var/log/exercice/recent_logs/
 
 for ((i=0;i<$1;i++))
 do
-    head -c 10 /dev/random    
-    #head -c 10 /dev/random > /var/log/exercice/recent_logs
+    TMP_FILE=$(mktemp -q /var/log/exercice/recent_logs/XXXXXX.log)
+    head -c 10 /dev/random > $TMP_FILE
+    echo $TMP_FILE
 done

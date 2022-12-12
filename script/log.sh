7c92828674f0565dbd00426e4569a9550f930c4e
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-12 12:06:57 +0100

    Creating the working directory

---
+++ script/log.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script write 10 caraters from /dev/random in n files
+# Each files have a unique name and will be save in /var/log/exercise/recent_logs
+
+sudo mkdir -p /var/log/exercice/recent_logs/
+

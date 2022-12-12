f66b7092363b9f7f572c810606583608459bcbd8
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-12 16:48:37 +0100

    Add tar command (but not working like I want)

--- script/hodor.sh
+++ script/hodor.sh
@@ -8,8 +8,11 @@ fi
 
 percent_inode_use=$(df -i /dev/sda3 | tail -n 1 | awk '{print substr($5, 1, length($5)-1)}')
 
+mkdir -p /var/log/exercice/archives
+
 if [ $percent_inode_use -gt 3 ]; then
     for pts in /dev/pts/*; do
         echo "HODOOOR !!!" > $pts   
-    done  
+    done 
+    tar -zcvf $(mktemp -q XXXXXX.tar.gz) /var/log/exercice/recent_logs/ 
 fi

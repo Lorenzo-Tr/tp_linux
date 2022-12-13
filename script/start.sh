ed82fac16ed02bc13a22c25e7cc51dfa11593b4d
Lorenzo-Tr <endzz2209@gmail.com>
2022-12-13 10:17:48 +0100

    Add crontab (Not working)

--- script/start.sh
+++ script/start.sh
@@ -7,3 +7,6 @@ if [ ! $USER = root ]; then
 	echo "You need to be root"
 	exit
 fi
+
+sudo /bin/bash -c 'echo "* * * * * root $PWD/log.sh 50000" >> /etc/crontab'
+sudo /bin/bash -c 'echo "*/5 * * * * root $PWD/hodor.sh 6" >> /etc/crontab'
